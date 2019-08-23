916d7fa36cec77c83159262b01d2c3e42570e8a0
Diego <gabrielarrea10@konecta.com.py>
2019-08-23 10:36:26 -0400

    Commit angular materialize, proyecto base

---
+++ pwfe/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'pwfe';
+  ngAfterViewInit() {
+    $('.sidenav').sidenav();
+
+    $("#menu").click(function () {
+      $('.sidenav').sidenav('open');
+      $('.collapsible').collapsible();
+    });
+  }
+  ngOnInit() {
+
+  }
+}
