7b6678026314da502b851f1a2d38f9fdf81dbf58
Diego Larrea <gabrielarrea10@gmail.com>
2019-08-26 23:26:17 -0400

    Fix action fixed button

--- pwfe/src/app/servicios/servicios.component.ts
+++ pwfe/src/app/servicios/servicios.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-
+declare var $:any;
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.css']
 })
 export class ServiciosComponent implements OnInit {
+  icon='add';
   data = [];
   searchText: string= '';
   constructor(private api: ApiService) {}
@@ -32,5 +33,10 @@ export class ServiciosComponent implements OnInit {
         console.log(error);
       }
     )
+  }
+
+  check(id){
+    $('a.agregar').addClass('red');
+    this.icon = 'delete';
   }
 }
