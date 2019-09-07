d056272bc9795e80b5332e4869664c195bb73c0c
Juancanhiza <juancahiza@gmail.com>
2019-09-07 12:25:32 -0400

    Mergee el modulo pacientes al master

--- pwfe/src/app/app-routing.module.ts
+++ pwfe/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoriaComponent } from './categoria/categoria.component';
 import { SubcategoriaComponent } from './subcategoria/subcategoria.component';
+import { PacientesComponent } from './pacientes/pacientes.component';
 
 const appRoutes: Routes = [
   { path: 'categorias', component:CategoriaComponent },
-  { path : 'subcategorias', component:SubcategoriaComponent }
+  { path: 'subcategorias', component:SubcategoriaComponent },
+  { path: 'pacientes', component:PacientesComponent },
 ];
 
 @NgModule({
